8007f5ccf6dfabdfc9774a7780ef70392b33356e
Сулименко Максим <91030653+mSulimenko@users.noreply.github.com>
2024-03-20 19:31:30 +0300

    Update EchoController.cs

--- server/Controllers/EchoController.cs
+++ server/Controllers/EchoController.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using BatteryDevicesMaster.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace BatteryDevicesMaster.Server.Controllers;
 
 /// <summary>
